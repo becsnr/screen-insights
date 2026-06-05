84426d3ba090e2ebc416a6df5509f3bd8ff7b7cc
Beca <234265987+becsnr@users.noreply.github.com>
2026-06-05 11:01:02 -0300

    background

---
+++ screen-insights/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import styles from './Dashboard.module.css'
+
+function Dashboard() {
+    return (
+        <main className={styles.container}>
+
+        </main>
+    )
+}
+
+export default Dashboard
