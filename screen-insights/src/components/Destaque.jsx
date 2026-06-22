9dc6dfda73b59ed79df2b6d14f545e6f585beb67
Beca <234265987+becsnr@users.noreply.github.com>
2026-06-22 13:52:02 -0300

    ajustes

--- screen-insights/src/components/Destaque.jsx
+++ screen-insights/src/components/Destaque.jsx
@@ -31,6 +31,7 @@ function Destaque() {
             icone: FaRegHeart,       
         }
     ]
+    
     return (
         <div className={styles.card}>
             <div className={styles.tituloCard}>
