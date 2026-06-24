5b18008eb67f1668adb2cc18b1ba9f87cad8e87e
Beca <234265987+becsnr@users.noreply.github.com>
2026-06-24 14:14:53 -0300

    mobile ajustes

--- screen-insights/src/components/Media.jsx
+++ screen-insights/src/components/Media.jsx
@@ -7,33 +7,44 @@ import { getMedia } from '../utils/getMedia';
 function Media({ data }) {
     const media = getMedia(data);
 
-    console.log(media);
+    const cards = [
+        {
+            titulo: "Desbloqueios",
+            total: media.unlocksTotal,
+            diaria: media.unlocks,
+            icone: FaLock
+        },
+        {
+            titulo: "Notificações",
+            total: media.notificationsTotal,
+            diaria: media.notifications,
+            icone: FaBell
+        }
+    ];
+
+    // console.log(media);
 
     return (
         <div className={styles.container}>
-            <div className={styles.card}>
-                <div className={styles.topo}>
-                    <div className={styles.icone}>
-                        <FaLock />
+            {cards.map(item => {
+                const Icone = item.icone;
+                
+                return (
+                    <div className={styles.card}>
+                        <div className={styles.topo}>
+                            <div className={styles.icone}>
+                                <Icone />
+                            </div>
+
+                            <h2>{item.titulo}</h2>
+                        </div>
+
+                        <h3>{item.total}</h3>
+
+                        <p>Média diária: {item.diaria}</p>
                     </div>
-                    <h2>Desbloqueios</h2>
-                </div>
-
-                <h3>{media.unlocksTotal}</h3>
-                <p>Média diária: {media.unlocks}</p>
-            </div>
-
-            <div className={styles.card}>
-                <div className={styles.topo}>
-                    <div className={styles.icone}>
-                        <FaBell />
-                    </div>
-                    <h2>Notificações</h2>
-                </div>
-
-                <h3>{media.notificationsTotal}</h3>
-                <p>Média diária: {media.notifications}</p>
-            </div>
+                )
+            })}
         </div>
     )
 }
